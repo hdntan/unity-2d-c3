16db29dd7d9eb8a3864b3638cb68664f807e33ed
= <=>
2025-02-14 16:27:57 +0700

    E27: add Fx

---
+++ Assets/_Data/Scripts/Fx/FxSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxSpawner : Spawner
+{
+    private static FxSpawner instance;
+    public static FxSpawner Instance => instance; 
+
+    public static string smoke1 = "Smoke_1";
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (FxSpawner.instance != null) Debug.LogError("Only 1 InputManager allow to exits");
+        FxSpawner.instance = this;
+    }
+}
