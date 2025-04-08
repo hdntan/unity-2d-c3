150196a896e62f0d05517f5af061231107adbf30
= <=>
2025-04-08 16:14:36 +0700

    E69: build test drop item

--- Assets/_Data/Scripts/Item/ItemDropTest.cs
+++ Assets/_Data/Scripts/Item/ItemDropTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,10 @@ using UnityEngine;
 public class ItemDropTest : MainMonoBehaviour
 {
     public JunkCtrl junkCtrl;
+    [SerializeField] protected int dropCount;
+    [SerializeField] protected List<ItemDropCount> itemDropCounts = new List<ItemDropCount>();
 
-    protected override void Start()
+     protected override void Start()
     {
         base.Start();
       InvokeRepeating(nameof(this.Dropping),2,0.5f);
@@ -14,8 +17,32 @@ public class ItemDropTest : MainMonoBehaviour
 
     protected virtual void Dropping()
     {
+        this.dropCount += 1;
         Vector3 dropPos = transform.position;
         Quaternion dropRota = transform.rotation;
-        ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObject.dropList, dropPos, dropRota);
+        List<ItemDropRate> itemDrops  = ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObject.dropList, dropPos, dropRota);
+        ItemDropCount itemDropCount;
+        foreach(ItemDropRate itemDrop in itemDrops)
+        {
+            itemDropCount = this.itemDropCounts.Find(item => item.itemName == itemDrop.ItemSO.itemName);
+            if(itemDropCount == null)
+            {
+                itemDropCount = new ItemDropCount();
+                itemDropCount.itemName = itemDrop.ItemSO.itemName;  
+                this.itemDropCounts.Add(itemDropCount);
+            }
+
+            itemDropCount.count += 1;
+            itemDropCount.rate = (float)Math.Round((float)itemDropCount.count / this.dropCount, 2);
+        }
     }
+}
+
+[Serializable]
+
+public class ItemDropCount
+{
+    public string itemName;
+    public int count;
+    public float rate;
 }
